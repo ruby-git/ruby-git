55c3c134c5c19103ed52fbd615d5af351fa9bb34
scott Chacon <schacon@agadorsparticus.corp.reactrix.com>
2007-11-20 12:08:11 -0800

    added a test for the logger

---
+++ tests/units/test_logger.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'logger'
+require File.dirname(__FILE__) + '/../test_helper'
+
+class TestLogger < Test::Unit::TestCase
+
+  def setup
+    set_file_paths
+  end
+  
+  def test_logger
+    log = Tempfile.new('logfile')
+    log.close
+    
+    logger = Logger.new(log.path)
+    logger.level = Logger::DEBUG
+    
+    @git = Git.open(@wdir, :log => logger)
+    @git.branches.size
+    
+    logc = File.read(log.path)
+    assert(/INFO -- : git branch -a/.match(logc))
+    assert(/DEBUG -- : \* git_grep/.match(logc))
+
+    log = Tempfile.new('logfile')
+    log.close
+    logger = Logger.new(log.path)
+    logger.level = Logger::INFO
+    
+    @git = Git.open(@wdir, :log => logger)
+    @git.branches.size
+    
+    logc = File.read(log.path)
+    assert(/INFO -- : git branch -a/.match(logc))
+    assert(!/DEBUG -- : \* git_grep/.match(logc))
+  end
+  
+end
