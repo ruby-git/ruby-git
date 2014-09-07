00a66cb498957eeda245618c8bf29c1d2e39a47a
bit <fruitcode@live.com>
2014-09-07 08:35:59 +0400

    Fix error when trying to get diff with nil parent

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -279,12 +279,14 @@ module Git
     def diff_stats(obj1 = 'HEAD', obj2 = nil, opts = {})
       diff_opts = ['--numstat']
       diff_opts << obj1
-      diff_opts << obj2 if obj2.is_a?(String)
+      diff_opts << obj2 if obj2.is_a?(String) && !obj2.empty?
       diff_opts << '--' << opts[:path_limiter] if opts[:path_limiter].is_a? String
 
       hsh = {:total => {:insertions => 0, :deletions => 0, :lines => 0, :files => 0}, :files => {}}
-      
-      command_lines('diff', diff_opts).each do |file|
+
+      diff_command = obj2.is_a?(String) && !obj2.empty? ? 'diff' : 'show'
+
+      command_lines(diff_command, diff_opts).each do |file|
         (insertions, deletions, filename) = file.split("\t")
         hsh[:total][:insertions] += insertions.to_i
         hsh[:total][:deletions] += deletions.to_i
