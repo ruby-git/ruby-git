e3bae27ba84461a5242ebb6f172425538b72857a
bit <fruitcode@live.com>
2014-09-14 18:02:14 +0400

    Keep original timezone in author/committer date

--- lib/git/author.rb
+++ lib/git/author.rb
@@ -3,10 +3,11 @@ module Git
     attr_accessor :name, :email, :date
     
     def initialize(author_string)
-      if m = /(.*?) <(.*?)> (\d+) (.*)/.match(author_string)
+      if m = /(.*?) <(.*?)> (\d+) ([-\+]\d+)/.match(author_string)
         @name = m[1]
         @email = m[2]
         @date = Time.at(m[3].to_i)
+        @date.localtime(m[4].gsub(/([-\+]\d{2})(\d{2})/, '\1:\2'))
       end
     end
     
