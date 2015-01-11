c05431c60e57880bb627a13fc1483b6f61d426ab
Jatin Ganhotra <jatin.ganhotra@gmail.com>
2015-01-11 14:15:12 -0600

    Using the Ruby 1.8 hash rocket syntax

--- lib/git/diff.rb
+++ lib/git/diff.rb
@@ -118,7 +118,7 @@ module Git
       def process_full_diff
         final = {}
         current_file = nil
-	full_diff_utf8_encoded = @full_diff.encode("UTF-8", "binary", invalid: :replace, undef: :replace)
+	full_diff_utf8_encoded = @full_diff.encode("UTF-8", "binary", :invalid => "replace", :undef => "replace")
 	full_diff_utf8_encoded.split("\n").each do |line|
           if m = /diff --git a\/(.*?) b\/(.*?)/.match(line)
             current_file = m[1]
