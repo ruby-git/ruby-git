199d7e422ee3c4aa66adc9664d31c19dd42b3fa7
José Tomás Albornoz <jojo@eljojo.net>
2015-06-28 14:30:00 +0200

    Passing block from #push to #command

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -716,7 +716,7 @@ module Git
       command('fetch', arr_opts)
     end
     
-    def push(remote, branch = 'master', opts = {})
+    def push(remote, branch = 'master', opts = {}, &block)
       # Small hack to keep backwards compatibility with the 'push(remote, branch, tags)' method signature.
       opts = {:tags => opts} if [true, false].include?(opts) 
       
@@ -724,8 +724,8 @@ module Git
       arr_opts << '--force'  if opts[:force] || opts[:f]
       arr_opts << remote
 
-      command('push', arr_opts + [branch])
-      command('push', ['--tags'] + arr_opts) if opts[:tags]
+      command('push', arr_opts + [branch], &block)
+      command('push', ['--tags'] + arr_opts, &block) if opts[:tags]
     end
 
     def pull(remote='origin', branch='master')
