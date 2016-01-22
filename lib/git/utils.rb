257d4afc4ffa7bcd27ac0dce1c69dd08bc5736ae
Dmitry Shmelev <avikez@gmail.com>
2016-01-22 11:07:14 +0300

    Refactoring of Git::Utils class to module; Git::Utils.url_to_ssh returns ssh link if input is ssh

--- lib/git/utils.rb
+++ lib/git/utils.rb
@@ -1,10 +1,21 @@
 require 'uri'
 
 module Git
-  class Utils
-    def self.url_to_ssh( url )
-      uri = URI( url )
-      "git@#{uri.host}:#{uri.path[1..-1]}"
+  module Utils
+    module_function
+
+    def is_web_url?( url)
+      return true if url =~ /\A#{URI::regexp(['http', 'https'])}\z/
+    end
+
+    def url_to_ssh( url )
+      if is_web_url? url
+        uri = URI( url )
+        org,repo = uri.path[1..-1].split('/')
+        "git@#{uri.host}:#{org}/#{repo}"
+      else
+        url
+      end
     end
   end
 end
