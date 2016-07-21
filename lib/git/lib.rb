78573ed1a440bfc80e492ae403a19bc7fa2f5f28
Nick Giancola <nick@gophilosophie.com>
2016-07-21 15:49:06 -0700

    Enable --follow option for git log

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -971,6 +971,7 @@ module Git
       arr_opts = []
 
       arr_opts << "-#{opts[:count]}" if opts[:count]
+      arr_opts << "--follow" if opts[:follow]
       arr_opts << "--no-color"
       arr_opts << "--since=#{opts[:since]}" if opts[:since].is_a? String
       arr_opts << "--until=#{opts[:until]}" if opts[:until].is_a? String
