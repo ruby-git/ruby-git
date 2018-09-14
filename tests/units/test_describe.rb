040628812c503acee56178bec0ea5881be1a5a94
Peter Cummuskey <peterc@aetheric.co.nz>
2018-09-14 13:19:45 +1200

    Adding test for --abbrev option usage.
    Signed-off-by: Peter Cummuskey <peterc@aetheric.co.nz>

--- tests/units/test_describe.rb
+++ tests/units/test_describe.rb
@@ -13,4 +13,20 @@ class TestDescribe < Test::Unit::TestCase
     assert_equal'v2.8', @git.describe(nil, tags: true)
   end
 
+  def test_describe_abbrev
+
+    # Make sure we're not on the tag.
+    @git.commit 'change', allow_empty: true
+
+    assert_match /^v2\.8-1-g\h{7}$/, @git.describe(nil, tags: true),
+        'git-describe should return the full describe if abbrev isn\'t provided'
+
+    assert_match /^v2\.8-1-g\h{1,8}$/, @git.describe(nil, tags: true, abbrev: 1),
+        'git-describe should use the minimum digits required for a unique object name.'
+
+    assert_equal 'v2.8', @git.describe(nil, tags: true, abbrev: 0),
+        'git-describe should just return the last tag if abbrev is 0'
+
+  end
+
 end
