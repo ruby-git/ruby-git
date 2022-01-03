934d17da36db2fabdfed384936ce2dfdd52abfc9
James Couball <jcouball@yahoo.com>
2022-01-03 08:35:25 -0800

    Add test for properly escaping double quote on Windows
    Signed-off-by: James Couball <jcouball@yahoo.com>

---
+++ tests/units/test_windows_cmd_escaping.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+# Test diff when the file path has to be quoted according to core.quotePath
+# See https://git-scm.com/docs/git-config#Documentation/git-config.txt-corequotePath
+#
+class TestWindowsCmdEscaping < Test::Unit::TestCase
+  def test_commit_with_double_quote_in_commit_message
+    expected_commit_message = 'Commit message with "double quotes"'
+    in_temp_dir do |path|
+      create_file('README.md', "# README\n")
+      git = Git.init('.')
+      git.add
+      git.commit(expected_commit_message)
+      commits = git.log(1)
+      actual_commit_message = commits.first.message
+      assert_equal(expected_commit_message, actual_commit_message)
+    end
+  end
+end
