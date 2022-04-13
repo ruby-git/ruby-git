018d919a6fe57f863e652a556b2e0dcf9bc47e9b
James Fairbairn <4-Eyes@users.noreply.github.com>
2022-04-14 08:08:01 +1200

    Fix bug when grepping lines that contain numbers surrounded by colons (#566)
    Signed-off-by: James Fairbairn <jamesaf112@gmail.com>

--- tests/units/test_describe.rb
+++ tests/units/test_describe.rb
@@ -3,14 +3,14 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TestDescribe < Test::Unit::TestCase
-  
+
   def setup
     set_file_paths
     @git = Git.open(@wdir)
   end
 
   def test_describe
-    assert_equal(@git.describe(nil, {:tags => true}), 'v2.8')
+    assert_equal(@git.describe(nil, {:tags => true}), 'grep_colon_numbers')
   end
 
 end
