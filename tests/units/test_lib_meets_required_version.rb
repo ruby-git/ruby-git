ca26ca900701ca4d85325a72461cc54b33433310
James Couball <jcouball@yahoo.com>
2023-01-11 13:33:05 -0800

    Fix version parsing
    Signed-off-by: James Couball <jcouball@yahoo.com>

--- tests/units/test_lib_meets_required_version.rb
+++ tests/units/test_lib_meets_required_version.rb
@@ -21,4 +21,31 @@ class TestLibMeetsRequiredVersion < Test::Unit::TestCase
     lib.define_singleton_method(:current_command_version) { [major_version, minor_version] }
     assert !lib.meets_required_version?
   end
+
+  def test_parse_version
+    lib = Git::Lib.new(nil, nil)
+
+    versions_to_test = [
+      { version_string: 'git version 2.1', expected_result: [2, 1, 0] },
+      { version_string: 'git version 2.28.4', expected_result: [2, 28, 4] },
+      { version_string: 'git version 2.32.GIT', expected_result: [2, 32, 0] },
+    ]
+
+    lib.instance_variable_set(:@next_version_index, 0)
+
+    lib.define_singleton_method(:command) do |cmd, *opts, &block|
+      raise ArgumentError unless cmd == 'version'
+      versions_to_test[@next_version_index][:version_string].tap { @next_version_index += 1 }
+    end
+
+    lib.define_singleton_method(:next_version_index) { @next_version_index }
+
+    expected_version = versions_to_test[lib.next_version_index][:expected_result]
+    actual_version = lib.current_command_version
+    assert_equal(expected_version, actual_version)
+
+    expected_version = versions_to_test[lib.next_version_index][:expected_result]
+    actual_version = lib.current_command_version
+    assert_equal(expected_version, actual_version)
+  end
 end
