1ccdbe0d7a500d50881b7f4dcfbae8b3046a1758
James Couball <jcouball@yahoo.com>
2023-02-27 17:17:43 -0800

    Rewrite worktree tests (#628)
    Signed-off-by: James Couball <jcouball@yahoo.com>

--- tests/units/test_thread_safety.rb
+++ tests/units/test_thread_safety.rb
@@ -7,6 +7,18 @@ class TestThreadSafety < Test::Unit::TestCase
     clone_working_repo
   end
 
+  teardown
+  def clean_environment
+    # TODO: this was needed because test_thread_safety.rb ocassionally leaks setting GIT_DIR.
+    # Once that is fixed, this can be removed.
+    # I think it will be fixed by using System.spawn or something similar instead
+    # of backticks to run git in Git::Lib#command.
+    ENV['GIT_DIR'] = nil
+    ENV['GIT_WORK_TREE'] = nil
+    ENV['GIT_INDEX_FILE'] = nil
+    ENV['GIT_SSH'] = nil
+  end
+
   def test_git_init_bare
     dirs = []
     threads = []
