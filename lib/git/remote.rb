7d8848c5257ca319e4587b35865889a816f0665b
James Couball <jcouball@yahoo.com>
2023-03-03 11:55:41 -0800

    Remote#branch and #merge should default to current branch instead of "master" (#639)
    Signed-off-by: James Couball <jcouball@yahoo.com>

--- lib/git/remote.rb
+++ lib/git/remote.rb
@@ -1,8 +1,8 @@
 module Git
   class Remote < Path
-    
+
     attr_accessor :name, :url, :fetch_opts
-    
+
     def initialize(base, name)
       @base = base
       config = @base.lib.config_remote(name)
@@ -10,27 +10,29 @@ module Git
       @url = config['url']
       @fetch_opts = config['fetch']
     end
-    
+
     def fetch(opts={})
       @base.fetch(@name, opts)
     end
-    
+
     # merge this remote locally
-    def merge(branch = 'master')
-      @base.merge("#{@name}/#{branch}")
+    def merge(branch = @base.current_branch)
+      remote_tracking_branch = "#{@name}/#{branch}"
+      @base.merge(remote_tracking_branch)
     end
-    
-    def branch(branch = 'master')
-      Git::Branch.new(@base, "#{@name}/#{branch}")
+
+    def branch(branch = @base.current_branch)
+      remote_tracking_branch = "#{@name}/#{branch}"
+      Git::Branch.new(@base, remote_tracking_branch)
     end
-    
+
     def remove
-      @base.lib.remote_remove(@name)     
+      @base.lib.remote_remove(@name)
     end
-    
+
     def to_s
       @name
     end
-    
+
   end
 end
