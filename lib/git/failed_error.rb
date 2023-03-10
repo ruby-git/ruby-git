5af1219b389fbd72dac8d8e3535fcd3b53279c17
James Couball <jcouball@yahoo.com>
2023-03-09 16:25:52 -0800

    Correctly report command output when there is an error (#658)
    Signed-off-by: James Couball <jcouball@yahoo.com>

--- lib/git/failed_error.rb
+++ lib/git/failed_error.rb
@@ -14,18 +14,20 @@ module Git
   class FailedError < Git::GitExecuteError
     # Create a FailedError object
     #
+    # Since this gem redirects stderr to stdout, the stdout of the process is used.
+    #
     # @example
     #   `exit 1` # set $? appropriately for this example
-    #   result = Git::CommandLineResult.new(%w[git status], $?, '', "failed")
+    #   result = Git::CommandLineResult.new(%w[git status], $?, 'stdout', 'stderr')
     #   error = Git::FailedError.new(result)
     #   error.message #=>
-    #     "[\"git\", \"status\"]\nstatus: pid 89784 exit 1\nstderr: \"failed\""
+    #     "[\"git\", \"status\"]\nstatus: pid 89784 exit 1\noutput: \"stdout\""
     #
     # @param result [Git::CommandLineResult] the result of the git command including
     #   the git command, status, stdout, and stderr
     #
     def initialize(result)
-      super("#{result.git_cmd}\nstatus: #{result.status}\nstderr: #{result.stderr.inspect}")
+      super("#{result.git_cmd}\nstatus: #{result.status}\noutput: #{result.stdout.inspect}")
       @result = result
     end
 
@@ -35,14 +37,14 @@ module Git
     #
     # @example
     #   `exit 1` # set $? appropriately for this example
-    #   result = Git::CommandLineResult.new(%w[git status], $?, '', "failed")
+    #   result = Git::CommandLineResult.new(%w[git status], $?, 'stdout', 'stderr')
     #   error = Git::FailedError.new(result)
     #   error.result #=>
     #     #<Git::CommandLineResult:0x00000001046bd488
     #       @git_cmd=["git", "status"],
     #       @status=#<Process::Status: pid 89784 exit 1>,
-    #       @stderr="failed",
-    #       @stdout="">
+    #       @stderr="stderr",
+    #       @stdout="stdout">
     #
     # @return [Git::CommandLineResult]
     #
