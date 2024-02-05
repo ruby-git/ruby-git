59a82a5694c4f52f32d43b8bb7c876ac7a74719f
James Couball <jcouball@yahoo.com>
2024-02-05 08:35:49 -0800

    Refactor the Error heriarchy (#693)
    * Refactor the Error heriarchy
    * Bump truffleruby to 24.0.0 to get support for endless methods
    
    Signed-off-by: James Couball <jcouball@yahoo.com>

--- lib/git/failed_error.rb
+++ lib/git/failed_error.rb
@@ -1,51 +1,14 @@
 # frozen_string_literal: true
 
-require 'git/git_execute_error'
+require_relative 'command_line_error'
 
 module Git
-  # This error is raised when a git command fails
+  # This error is raised when a git command returns a non-zero exitstatus
   #
   # The git command executed, status, stdout, and stderr are available from this
-  # object. The #message includes the git command, the status of the process, and
-  # the stderr of the process.
+  # object.
   #
   # @api public
   #
-  class FailedError < Git::GitExecuteError
-    # Create a FailedError object
-    #
-    # @example
-    #   `exit 1` # set $? appropriately for this example
-    #   result = Git::CommandLineResult.new(%w[git status], $?, 'stdout', 'stderr')
-    #   error = Git::FailedError.new(result)
-    #   error.message #=>
-    #     "[\"git\", \"status\"]\nstatus: pid 89784 exit 1\nstderr: \"stderr\""
-    #
-    # @param result [Git::CommandLineResult] the result of the git command including
-    #   the git command, status, stdout, and stderr
-    #
-    def initialize(result)
-      super("#{result.git_cmd}\nstatus: #{result.status}\nstderr: #{result.stderr.inspect}")
-      @result = result
-    end
-
-    # @attribute [r] result
-    #
-    # The result of the git command including the git command and its status and output
-    #
-    # @example
-    #   `exit 1` # set $? appropriately for this example
-    #   result = Git::CommandLineResult.new(%w[git status], $?, 'stdout', 'stderr')
-    #   error = Git::FailedError.new(result)
-    #   error.result #=>
-    #     #<Git::CommandLineResult:0x00000001046bd488
-    #       @git_cmd=["git", "status"],
-    #       @status=#<Process::Status: pid 89784 exit 1>,
-    #       @stderr="stderr",
-    #       @stdout="stdout">
-    #
-    # @return [Git::CommandLineResult]
-    #
-    attr_reader :result
-  end
+  class FailedError < Git::CommandLineError; end
 end
---
+++ tests/units/test_timeout_error.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TestTimeoutError < Test::Unit::TestCase
+  def test_initializer
+    status = Struct.new(:to_s).new('pid 65628 SIGKILL (signal 9)') # `kill -9 $$`
+    result = Git::CommandLineResult.new(%w[git status], status, 'stdout', 'stderr')
+    timeout_diration = 10
+
+    error = Git::TimeoutError.new(result, timeout_diration)
+
+    assert(error.is_a?(Git::SignaledError))
+  end
+
+  def test_to_s
+    status = Struct.new(:to_s).new('pid 65628 SIGKILL (signal 9)') # `kill -9 $$`
+    result = Git::CommandLineResult.new(%w[git status], status, 'stdout', 'Waiting...')
+    timeout_duration = 10
+
+    error = Git::TimeoutError.new(result, timeout_duration)
+
+    expected_message = '["git", "status"], status: pid 65628 SIGKILL (signal 9), stderr: "Waiting...", timed out after 10s'
+    assert_equal(expected_message, error.to_s)
+  end
+end
