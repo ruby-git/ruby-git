2e79dbe657ae66905402dc663d6efbc18e445d16
Costa Shapiro <costa@mouldwarp.com>
2024-10-23 11:23:08 +0300

    Fixed "unbranched" stash message support:
    - the tests are generously provided by James Couball <jcouball@yahoo.com>
    - more proper stash metadata parsing introduced
      - supporting both "branched" ("On <BRANCH>: ...") and "unbranched" messages
      - which might affect the future 3.x behaviour wrt "un/branched" stashes

--- tests/units/test_stashes.rb
+++ tests/units/test_stashes.rb
@@ -44,4 +44,107 @@ class TestStashes < Test::Unit::TestCase
       assert(stashes[0].include?('testing-stash-all'))
     end
   end
+  test 'Git::Lib#stashes_all' do
+    in_bare_repo_clone do |g|
+      assert_equal(0, g.branch.stashes.size)
+      new_file('test-file1', 'blahblahblah1')
+      new_file('test-file2', 'blahblahblah2')
+      assert(g.status.untracked.assoc('test-file1'))
+
+      g.add
+
+      assert(g.status.added.assoc('test-file1'))
+
+      g.branch.stashes.save('testing-stash-all')
+
+      # puts `cat .git/logs/refs/stash`
+      # 0000000000000000000000000000000000000000 b9b008cd179b0e8c4b8cda35bac43f7011a0836a James Couball <jcouball@yahoo.com> 1729463252 -0700   On master: testing-stash-all
+
+      stashes = assert_nothing_raised { g.lib.stashes_all }
+
+      expected_stashes = [
+        [0, 'testing-stash-all']
+      ]
+
+      assert_equal(expected_stashes, stashes)
+    end
+  end
+
+  test 'Git::Lib#stashes_all - stash message has colon' do
+    in_bare_repo_clone do |g|
+      assert_equal(0, g.branch.stashes.size)
+      new_file('test-file1', 'blahblahblah1')
+      new_file('test-file2', 'blahblahblah2')
+      assert(g.status.untracked.assoc('test-file1'))
+
+      g.add
+
+      assert(g.status.added.assoc('test-file1'))
+
+      g.branch.stashes.save('saving: testing-stash-all')
+
+      # puts `cat .git/logs/refs/stash`
+      # 0000000000000000000000000000000000000000 b9b008cd179b0e8c4b8cda35bac43f7011a0836a James Couball <jcouball@yahoo.com> 1729463252 -0700   On master: saving: testing-stash-all
+
+      stashes = assert_nothing_raised { g.lib.stashes_all }
+
+      expected_stashes = [
+        [0, 'saving: testing-stash-all']
+      ]
+
+      assert_equal(expected_stashes, stashes)
+    end
+  end
+
+  test 'Git::Lib#stashes_all -- git stash message with no branch and no colon' do
+    in_temp_dir do
+      `git init`
+      `echo "hello world" > file1.txt`
+      `git add file1.txt`
+      `git commit -m "First commit"`
+      `echo "update" > file1.txt`
+      commit = `git stash create "stash message"`.chomp
+      # Create a stash with this message: 'custom message'
+      `git stash store -m "custom message" #{commit}`
+
+      # puts `cat .git/logs/refs/stash`
+      # 0000000000000000000000000000000000000000 0550a54ed781eda364ca3c22fcc46c37acae4bd6 James Couball <jcouball@yahoo.com> 1729460302 -0700   custom message
+
+      git = Git.open('.')
+
+      stashes = assert_nothing_raised { git.lib.stashes_all }
+
+      expected_stashes = [
+        [0, 'custom message']
+      ]
+
+      assert_equal(expected_stashes, stashes)
+    end
+  end
+
+  test 'Git::Lib#stashes_all -- git stash message with no branch and explicit colon' do
+    in_temp_dir do
+      `git init`
+      `echo "hello world" > file1.txt`
+      `git add file1.txt`
+      `git commit -m "First commit"`
+      `echo "update" > file1.txt`
+      commit = `git stash create "stash message"`.chomp
+      # Create a stash with this message: 'custom message'
+      `git stash store -m "testing: custom message" #{commit}`
+
+      # puts `cat .git/logs/refs/stash`
+      # 0000000000000000000000000000000000000000 eadd7858e53ea4fb8b1383d69cade1806d948867 James Couball <jcouball@yahoo.com> 1729462039 -0700   testing: custom message
+
+      git = Git.open('.')
+
+      stashes = assert_nothing_raised { git.lib.stashes_all }
+
+      expected_stashes = [
+        [0, 'custom message']
+      ]
+
+      assert_equal(expected_stashes, stashes)
+    end
+  end
 end
