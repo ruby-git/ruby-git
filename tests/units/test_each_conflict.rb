b3cd76c7802172bff1c95e8debad4058ae29f730
James Couball <jcouball@yahoo.com>
2025-07-03 09:16:57 -0700

    fix: fix Rubocop Lint/SuppressedException offense

--- tests/units/test_each_conflict.rb
+++ tests/units/test_each_conflict.rb
@@ -5,9 +5,10 @@ require 'test_helper'
 class TestEachConflict < Test::Unit::TestCase
   def test_conflicts
     in_temp_repo('working') do
+      # Setup a repository with a conflict
       g = Git.open('.')
 
-      g.branch('new_branch').in_branch('test') do
+      g.branch('new_branch').in_branch('commit message') do
         new_file('example.txt', "1\n2\n3")
         g.add
         true
@@ -20,11 +21,17 @@ class TestEachConflict < Test::Unit::TestCase
       end
 
       g.merge('new_branch')
+
       begin
         g.merge('new_branch2')
-      rescue StandardError
+      rescue Git::FailedError => e
+        assert_equal(1, e.result.status.exitstatus)
+        assert_match(/CONFLICT/, e.result.stdout)
       end
 
+      assert_equal(1, g.lib.unmerged.size)
+
+      # Check the conflict
       g.each_conflict do |file, your, their|
         assert_equal('example.txt', file)
         assert_equal("1\n2\n3\n", File.read(your))
