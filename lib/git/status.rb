0e9d62db06365959365296a5a9b7d35fb3c7631b
James Couball <jcouball@yahoo.com>
2025-07-06 13:09:33 -0700

    fix: fix Rubocop Metrics/ClassLength offense (refactor Git::Status)
    This refactoring streamlines the Git::Status class by decomposing
    its responsibilities, resulting in cleaner, more focused components:
    
    * `StatusFile`: The inner data responsibilities was moved to its
      own class.
    * `StatusFileFactory`: A new private factory was created to
      encapsulate all the logic for executing git commands and parsing
      their output into StatusFile objects.
    
    I think the result is more readable and maintainable code.

--- lib/git/status.rb
+++ lib/git/status.rb
@@ -1,114 +1,47 @@
 # frozen_string_literal: true
 
+# These would be required by the main `git.rb` file
+
 module Git
-  # The status class gets the status of a git repository
-  #
-  # This identifies which files have been modified, added, or deleted from the
-  # worktree. Untracked files are also identified.
-  #
-  # The Status object is an Enumerable that contains StatusFile objects.
+  # The Status class gets the status of a git repository. It identifies which
+  # files have been modified, added, or deleted, including untracked files.
+  # The Status object is an Enumerable of StatusFile objects.
   #
   # @api public
   #
   class Status
     include Enumerable
 
+    # @param base [Git::Base] The base git object
     def initialize(base)
       @base = base
-      construct_status
+      # The factory returns a hash of file paths to StatusFile objects.
+      @files = StatusFileFactory.new(base).construct_files
     end
 
-    #
-    # Returns an Enumerable containing files that have changed from the
-    # git base directory
-    #
-    # @return [Enumerable]
-    def changed
-      @changed ||= @files.select { |_k, f| f.type == 'M' }
-    end
+    # File status collections, memoized for performance.
+    def changed   = @changed ||= select_files { |f| f.type == 'M' }
+    def added     = @added ||= select_files { |f| f.type == 'A' }
+    def deleted   = @deleted ||= select_files { |f| f.type == 'D' }
+    # This works with `true` or `nil`
+    def untracked = @untracked ||= select_files(&:untracked)
 
-    #
-    # Determines whether the given file has been changed.
-    # File path starts at git base directory
-    #
-    # @param file [String] The name of the file.
-    # @example Check if lib/git.rb has changed.
-    #     changed?('lib/git.rb')
-    # @return [Boolean]
-    def changed?(file)
-      case_aware_include?(:changed, :lc_changed, file)
-    end
+    # Predicate methods to check the status of a specific file.
+    def changed?(file)   = file_in_collection?(:changed, file)
+    def added?(file)     = file_in_collection?(:added, file)
+    def deleted?(file)   = file_in_collection?(:deleted, file)
+    def untracked?(file) = file_in_collection?(:untracked, file)
 
-    # Returns an Enumerable containing files that have been added.
-    # File path starts at git base directory
-    #
-    # @return [Enumerable]
-    def added
-      @added ||= @files.select { |_k, f| f.type == 'A' }
-    end
+    # Access a status file by path, or iterate over all status files.
+    def [](file) = @files[file]
+    def each(&) = @files.values.each(&)
 
-    # Determines whether the given file has been added to the repository
-    #
-    # File path starts at git base directory
-    #
-    # @param file [String] The name of the file.
-    # @example Check if lib/git.rb is added.
-    #     added?('lib/git.rb')
-    # @return [Boolean]
-    def added?(file)
-      case_aware_include?(:added, :lc_added, file)
-    end
-
-    #
-    # Returns an Enumerable containing files that have been deleted.
-    # File path starts at git base directory
-    #
-    # @return [Enumerable]
-    def deleted
-      @deleted ||= @files.select { |_k, f| f.type == 'D' }
-    end
-
-    #
-    # Determines whether the given file has been deleted from the repository
-    # File path starts at git base directory
-    #
-    # @param file [String] The name of the file.
-    # @example Check if lib/git.rb is deleted.
-    #     deleted?('lib/git.rb')
-    # @return [Boolean]
-    def deleted?(file)
-      case_aware_include?(:deleted, :lc_deleted, file)
-    end
-
-    #
-    # Returns an Enumerable containing files that are not tracked in git.
-    # File path starts at git base directory
-    #
-    # @return [Enumerable]
-    def untracked
-      @untracked ||= @files.select { |_k, f| f.untracked }
-    end
-
-    #
-    # Determines whether the given file is tracked by git.
-    # File path starts at git base directory
-    #
-    # @param file [String] The name of the file.
-    # @example Check if lib/git.rb is an untracked file.
-    #     untracked?('lib/git.rb')
-    # @return [Boolean]
-    def untracked?(file)
-      case_aware_include?(:untracked, :lc_untracked, file)
-    end
-
+    # Returns a formatted string representation of the status.
     def pretty
-      out = +''
-      each do |file|
-        out << pretty_file(file)
-      end
-      out << "\n"
-      out
+      map { |file| pretty_file(file) }.join << "\n"
     end
+
+    private
 
     def pretty_file(file)
       <<~FILE
@@ -121,198 +54,115 @@ module Git
       FILE
     end
 
-    # enumerable method
-
-    def [](file)
-      @files[file]
+    def select_files(&block)
+      @files.select { |_path, file| block.call(file) }
     end
 
-    def each(&)
-      @files.values.each(&)
-    end
-
-    # subclass that does heavy lifting
-    class StatusFile
-      # @!attribute [r] path
-      #   The path of the file relative to the project root directory
-      #   @return [String]
-      attr_accessor :path
-
-      # @!attribute [r] type
-      #   The type of change
-      #
-      #   * 'M': modified
-      #   * 'A': added
-      #   * 'D': deleted
-      #   * nil: ???
-      #
-      #   @return [String]
-      attr_accessor :type
-
-      # @!attribute [r] mode_index
-      #   The mode of the file in the index
-      #   @return [String]
-      #   @example 100644
-      #
-      attr_accessor :mode_index
-
-      # @!attribute [r] mode_repo
-      #   The mode of the file in the repo
-      #   @return [String]
-      #   @example 100644
-      #
-      attr_accessor :mode_repo
-
-      # @!attribute [r] sha_index
-      #   The sha of the file in the index
-      #   @return [String]
-      #   @example 123456
-      #
-      attr_accessor :sha_index
-
-      # @!attribute [r] sha_repo
-      #   The sha of the file in the repo
-      #   @return [String]
-      #   @example 123456
-      attr_accessor :sha_repo
-
-      # @!attribute [r] untracked
-      #   Whether the file is untracked
-      #   @return [Boolean]
-      attr_accessor :untracked
-
-      # @!attribute [r] stage
-      #   The stage of the file
-      #
-      #   * '0': the unmerged state
-      #   * '1': the common ancestor (or original) version
-      #   * '2': "our version" from the current branch head
-      #   * '3': "their version" from the other branch head
-      #   @return [String]
-      attr_accessor :stage
-
-      def initialize(base, hash)
-        @base = base
-        @path = hash[:path]
-        @type = hash[:type]
-        @stage = hash[:stage]
-        @mode_index = hash[:mode_index]
-        @mode_repo = hash[:mode_repo]
-        @sha_index = hash[:sha_index]
-        @sha_repo = hash[:sha_repo]
-        @untracked = hash[:untracked]
-      end
-
-      def blob(type = :index)
-        if type == :repo
-          @base.object(@sha_repo)
-        else
-          begin
-            @base.object(@sha_index)
-          rescue StandardError
-            @base.object(@sha_repo)
-          end
-        end
+    def file_in_collection?(collection_name, file_path)
+      collection = public_send(collection_name)
+      if ignore_case?
+        downcased_keys(collection_name).include?(file_path.downcase)
+      else
+        collection.key?(file_path)
       end
     end
 
-    private
-
-    def construct_status
-      # Lists all files in the index and the worktree
-      # git ls-files --stage
-      # { file => { path: file, mode_index: '100644', sha_index: 'dd4fc23', stage: '0' } }
-      @files = @base.lib.ls_files
-
-      # Lists files in the worktree that are not in the index
-      # Add untracked files to @files
-      fetch_untracked
-
-      # Lists files that are different between the index vs. the worktree
-      fetch_modified
-
-      # Lists files that are different between the repo HEAD vs. the worktree
-      fetch_added
-
-      @files.each do |k, file_hash|
-        @files[k] = StatusFile.new(@base, file_hash)
-      end
+    def downcased_keys(collection_name)
+      @_downcased_keys ||= {}
+      @_downcased_keys[collection_name] ||=
+        public_send(collection_name).keys.to_set(&:downcase)
     end
 
-    def fetch_untracked
-      # git ls-files --others --exclude-standard, chdir: @git_work_dir)
-      # { file => { path: file, untracked: true } }
-      @base.lib.untracked_files.each do |file|
-        @files[file] = { path: file, untracked: true }
-      end
-    end
-
-    def fetch_modified
-      # Files changed between the index vs. the worktree
-      # git diff-files
-      # {
-      #   file => {
-      #     path: file, type: 'M', mode_index: '100644', mode_repo: '100644',
-      #     sha_index: '0000000', :sha_repo: '52c6c4e'
-      #   }
-      # }
-      @base.lib.diff_files.each do |path, data|
-        @files[path] ? @files[path].merge!(data) : @files[path] = data
-      end
-    end
-
-    def fetch_added
-      return if @base.lib.empty?
-
-      # Files changed between the repo HEAD vs. the worktree
-      # git diff-index HEAD
-      # {
-      #   file => {
-      #     path: file, type: 'M', mode_index: '100644', mode_repo: '100644',
-      #     sha_index: '0000000', :sha_repo: '52c6c4e'
-      #   }
-      # }
-      @base.lib.diff_index('HEAD').each do |path, data|
-        @files[path] ? @files[path].merge!(data) : @files[path] = data
-      end
-    end
-
-    # It's worth noting that (like git itself) this gem will not behave well if
-    # ignoreCase is set inconsistently with the file-system itself. For details:
-    # https://git-scm.com/docs/git-config#Documentation/git-config.txt-coreignoreCase
     def ignore_case?
       return @_ignore_case if defined?(@_ignore_case)
 
-      @_ignore_case = @base.config('core.ignoreCase') == 'true'
+      @_ignore_case = (@base.config('core.ignoreCase') == 'true')
     rescue Git::FailedError
       @_ignore_case = false
     end
+  end
+end
 
-    def downcase_keys(hash)
-      hash.transform_keys(&:downcase)
+module Git
+  class Status
+    # Represents a single file's status in the git repository. Each instance
+    # holds information about a file's state in the index and working tree.
+    class StatusFile
+      attr_reader :path, :type, :stage, :mode_index, :mode_repo,
+                  :sha_index, :sha_repo, :untracked
+
+      def initialize(base, hash)
+        @base       = base
+        @path       = hash[:path]
+        @type       = hash[:type]
+        @stage      = hash[:stage]
+        @mode_index = hash[:mode_index]
+        @mode_repo  = hash[:mode_repo]
+        @sha_index  = hash[:sha_index]
+        @sha_repo   = hash[:sha_repo]
+        @untracked  = hash[:untracked]
+      end
+
+      # Returns a Git::Object::Blob for either the index or repo version of the file.
+      def blob(type = :index)
+        sha = type == :repo ? sha_repo : (sha_index || sha_repo)
+        @base.object(sha) if sha
+      end
     end
+  end
+end
 
-    def lc_changed
-      @lc_changed ||= changed.transform_keys(&:downcase)
-    end
+module Git
+  class Status
+    # A factory class responsible for fetching git status data and building
+    # a hash of StatusFile objects.
+    # @api private
+    class StatusFileFactory
+      def initialize(base)
+        @base = base
+        @lib = base.lib
+      end
 
-    def lc_added
-      @lc_added ||= added.transform_keys(&:downcase)
-    end
+      # Gathers all status data and builds a hash of file paths to
+      # StatusFile objects.
+      def construct_files
+        files_data = fetch_all_files_data
+        files_data.transform_values do |data|
+          StatusFile.new(@base, data)
+        end
+      end
 
-    def lc_deleted
-      @lc_deleted ||= deleted.transform_keys(&:downcase)
-    end
+      private
 
-    def lc_untracked
-      @lc_untracked ||= untracked.transform_keys(&:downcase)
-    end
+      # Fetches and merges status information from multiple git commands.
+      def fetch_all_files_data
+        files = @lib.ls_files # Start with files tracked in the index.
+        merge_untracked_files(files)
+        merge_modified_files(files)
+        merge_head_diffs(files)
+        files
+      end
 
-    def case_aware_include?(cased_hash, downcased_hash, file)
-      if ignore_case?
-        send(downcased_hash).include?(file.downcase)
-      else
-        send(cased_hash).include?(file)
+      def merge_untracked_files(files)
+        @lib.untracked_files.each do |file|
+          files[file] = { path: file, untracked: true }
+        end
+      end
+
+      def merge_modified_files(files)
+        # Merge changes between the index and the working directory.
+        @lib.diff_files.each do |path, data|
+          (files[path] ||= {}).merge!(data)
+        end
+      end
+
+      def merge_head_diffs(files)
+        return if @lib.empty?
+
+        # Merge changes between HEAD and the index.
+        @lib.diff_index('HEAD').each do |path, data|
+          (files[path] ||= {}).merge!(data)
+        end
       end
     end
   end
