9f0f50ecbbe82552876dda31647963fea138142a
James Couball <jcouball@yahoo.com>
2026-03-25 18:17:30 -0700

    refactor(lib): delegate unmerged to Git::Commands::Diff

--- tests/units/test_each_conflict.rb
+++ tests/units/test_each_conflict.rb
@@ -3,6 +3,13 @@
 require 'test_helper'
 
 class TestEachConflict < Test::Unit::TestCase
+  def test_unmerged_returns_empty_when_no_conflicts
+    in_temp_repo('working') do
+      g = Git.open('.')
+      assert_equal([], g.lib.unmerged)
+    end
+  end
+
   def test_conflicts
     in_temp_repo('working') do
       # Setup a repository with a conflict
@@ -29,7 +36,7 @@ class TestEachConflict < Test::Unit::TestCase
         assert_match(/CONFLICT/, e.result.stdout)
       end
 
-      assert_equal(1, g.lib.unmerged.size)
+      assert_equal(['example.txt'], g.lib.unmerged)
 
       # Check the conflict
       g.each_conflict do |file, your, their|
@@ -37,6 +44,36 @@ class TestEachConflict < Test::Unit::TestCase
         assert_equal("1\n2\n3\n", File.read(your))
         assert_equal("1\n4\n3\n", File.read(their))
       end
+    end
+  end
+
+  def test_unmerged_returns_all_conflicting_files
+    in_temp_repo('working') do
+      g = Git.open('.')
+
+      g.branch('branch_a').in_branch('branch a changes') do
+        new_file('file1.txt', "a\nb\nc")
+        new_file('file2.txt', "x\ny\nz")
+        g.add
+        true
+      end
+
+      g.branch('branch_b').in_branch('branch b changes') do
+        new_file('file1.txt', "a\nB\nc")
+        new_file('file2.txt', "x\nY\nz")
+        g.add
+        true
+      end
+
+      g.merge('branch_a')
+
+      begin
+        g.merge('branch_b')
+      rescue Git::FailedError
+        # expected conflict
+      end
+
+      assert_equal(['file1.txt', 'file2.txt'], g.lib.unmerged.sort)
     end
   end
 end
