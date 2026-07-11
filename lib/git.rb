b40da0176e7b148add29431ce62da4adedcee8e8
James Couball <jcouball@yahoo.com>
2026-07-11 12:59:29 -0700

    feat: allow deprecation behavior env var
    Add support for a GIT_DEPRECATION_BEHAVIOR environment variable that
    configures the ActiveSupport deprecation behavior when the gem is
    loaded. Any behavior name accepted by the installed ActiveSupport
    version is valid (e.g. 'silence', 'log', 'raise', 'report').
    
    If the variable is set to an unrecognised value, loading the gem raises
    ArgumentError with the list of accepted values.
    
    Also marks Git::Deprecation as @api public since users need to
    reference it to configure deprecation handling in code.

--- lib/git.rb
+++ lib/git.rb
@@ -1,10 +1,29 @@
 # frozen_string_literal: true
 
-require 'active_support'
 require 'active_support/deprecation'
 
+# Define Git::Deprecation before requiring the rest of the library to ensure that
+# any deprecation warnings emitted during the loading of the library are properly
+# configured according to the GIT_DEPRECATION_BEHAVIOR environment variable.
+#
 module Git
+  # The deprecation instance used to emit deprecation warnings for the Git gem
+  #
+  # @api public
   Deprecation = ActiveSupport::Deprecation.new('5.0.0', 'Git')
+
+  if (behavior = ENV.fetch('GIT_DEPRECATION_BEHAVIOR', nil))
+    behavior = behavior.strip
+    allowed_behaviors = ActiveSupport::Deprecation::DEFAULT_BEHAVIORS.keys.map(&:to_s)
+
+    unless allowed_behaviors.include?(behavior)
+      raise ArgumentError,
+            "Invalid GIT_DEPRECATION_BEHAVIOR=#{behavior.inspect}; " \
+            "expected one of: #{allowed_behaviors.join(', ')}"
+    end
+
+    Deprecation.behavior = behavior.to_sym
+  end
 end
 
 require 'git/author'
