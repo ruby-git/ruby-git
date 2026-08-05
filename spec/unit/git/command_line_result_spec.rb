05c3c4350e18bf1403fa314902708cfc40848c70
James Couball <jcouball@yahoo.com>
2026-08-05 13:27:20 -0700

    test: cover Git.const_missing fallback branch for unrelated constants
    The unless branch of const_missing (the NameError fallback for
    constants other than :CommandLineResult) had no unit coverage.

--- spec/unit/git/command_line_result_spec.rb
+++ spec/unit/git/command_line_result_spec.rb
@@ -11,13 +11,21 @@ RSpec.describe 'Git::CommandLineResult (deprecated)' do
     Git.send(:remove_const, :CommandLineResult) if Git.const_defined?(:CommandLineResult, false)
   end
 
-  it 'resolves to Git::CommandLine::Result' do
-    allow(Git::Deprecation).to receive(:warn)
-    expect(Git::CommandLineResult).to be(Git::CommandLine::Result)
+  context 'when accessing Git::CommandLineResult' do
+    it 'resolves to Git::CommandLine::Result' do
+      allow(Git::Deprecation).to receive(:warn)
+      expect(Git::CommandLineResult).to be(Git::CommandLine::Result)
+    end
+
+    it 'emits a deprecation warning when accessed' do
+      expect(Git::Deprecation).to receive(:warn).with(/Git::CommandLineResult is deprecated/)
+      Git::CommandLineResult
+    end
   end
 
-  it 'emits a deprecation warning when accessed' do
-    expect(Git::Deprecation).to receive(:warn).with(/Git::CommandLineResult is deprecated/)
-    Git::CommandLineResult
+  context 'when accessing an unrelated missing constant' do
+    it 'raises NameError instead of resolving it' do
+      expect { Git::NotARealConstant }.to raise_error(NameError, /uninitialized constant Git::NotARealConstant/)
+    end
   end
 end
