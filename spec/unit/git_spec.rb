dc3b57686b224bf382260787496c7f7b67640c5d
James Couball <jcouball@yahoo.com>
2026-08-05 13:27:24 -0700

    test: cover Git.export
    Git.export had no unit coverage, leaving the method body and both
    branches of the conditional origin/#{branch} checkout unexercised.

--- spec/unit/git_spec.rb
+++ spec/unit/git_spec.rb
@@ -48,4 +48,55 @@ RSpec.describe Git do
       end
     end
   end
+
+  describe '.export' do
+    subject(:result) { described_class.export(repository_url, directory, options) }
+
+    let(:repository_url) { 'https://example.com/repo.git' }
+    let(:directory) { '/tmp/example' }
+    let(:options) { {} }
+    let(:repo) { instance_double(Git::Repository, dir: Pathname.new(directory)) }
+
+    before do
+      allow(described_class).to receive(:clone).and_return(repo)
+      allow(FileUtils).to receive(:rm_r)
+    end
+
+    it 'clones the repository with depth: 1 merged into the given options' do
+      expect(described_class).to receive(:clone).with(repository_url, directory, { depth: 1 }).and_return(repo)
+      result
+    end
+
+    it 'removes the .git directory from the cloned repository' do
+      expect(FileUtils).to receive(:rm_r).with(File.join(directory, '.git'))
+      result
+    end
+
+    context 'when options include :remote' do
+      let(:options) { { remote: 'upstream' } }
+
+      it 'removes :remote before passing options to clone' do
+        expect(described_class).to receive(:clone).with(repository_url, directory, { depth: 1 }).and_return(repo)
+        result
+      end
+    end
+
+    context 'when options include :branch' do
+      let(:options) { { branch: 'develop' } }
+
+      before { allow(repo).to receive(:checkout) }
+
+      it 'checks out the origin branch on the cloned repository' do
+        expect(repo).to receive(:checkout).with('origin/develop')
+        result
+      end
+    end
+
+    context 'when options do not include :branch' do
+      it 'does not check out a branch' do
+        expect(repo).not_to receive(:checkout)
+        result
+      end
+    end
+  end
 end
