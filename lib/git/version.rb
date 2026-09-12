8e5ae409c735963798377ec0033d53e87fb72785
James Couball <jcouball@yahoo.com>
2026-09-12 16:33:12 -0700

    chore: release v5.6.0

--- lib/git/version.rb
+++ lib/git/version.rb
@@ -4,7 +4,7 @@ module Git
   # The current gem version
   #
   # @return [String] the current gem version
-  VERSION = '5.5.0'
+  VERSION = '5.6.0'
 
   # Represents a git version with major, minor, and patch components
   #
